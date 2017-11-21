2222ff7622b6317ebb6a64270bc2d085fe706c00
Lloyd Yeo <ywz.lloyd@gmail.com>
2017-11-22 04:54:29 +0800

    Web: Landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Stack Multipurpose HTML Template</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Site Description Here">
+		<link href="stack/css/bootstrap.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="stack/css/stack-interface.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="stack/css/socicon.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="stack/css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="stack/css/flickity.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="stack/css/iconsmind.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="stack/css/jquery.steps.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="stack/css/theme.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="stack/css/custom.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i"
+		      rel="stylesheet">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	</head>
+	<body class=" ">
+	<a id="start"></a>
+	<div class="nav-container ">
+		<div class="bar bar--sm visible-xs">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<a href="index.html">
+							<img class="logo logo-dark" alt="logo" src="img/logo-dark.png"/>
+							<img class="logo logo-light" alt="logo" src="img/logo-light.png"/>
+						</a>
+					</div>
+					<div class="col-xs-9 col-sm-10 text-right">
+						<a href="#" class="hamburger-toggle" data-toggle-class="#menu2;hidden-xs hidden-sm">
+							<i class="icon icon--sm stack-interface stack-menu"></i>
+						</a>
+					</div>
+				</div>
+				<!--end of row-->
+			</div>
+			<!--end of container-->
+		</div>
+		<!--end bar-->
+		<nav id="menu2" class="bar bar-2 hidden-xs bar--absolute bar--transparent">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2 text-center text-left-sm hidden-xs col-md-push-5">
+						<div class="bar__module">
+							<a href="/">
+								<img class="logo logo-dark" alt="logo here" src="img/logo-dark.png"/>
+								<img class="logo logo-light" alt="logo here" src="img/logo-light.png"/>
+							</a>
+						</div>
+						<!--end module-->
+					</div>
+					<div class="col-md-5 col-md-pull-2">
+						<div class="bar__module">
+							<ul class="menu-horizontal text-left">
+							</ul>
+						</div>
+						<!--end module-->
+					</div>
+					<div class="col-md-5 text-right text-left-xs text-left-sm">
+						<div class="bar__module">
+							<a class="btn btn--sm type--uppercase" href="/login">
+                                    <span class="btn__text">
+                                        LOGIN
+                                    </span>
+							</a>
+							<a class="btn btn--sm btn--primary type--uppercase" href="/signup">
+                                    <span class="btn__text">
+                                        SIGN UP NOW
+                                    </span>
+							</a>
+						</div>
+						<!--end module-->
+					</div>
+				</div>
+				<!--end of row-->
+			</div>
+			<!--end of container-->
+		</nav>
+		<!--end bar-->
+	</div>
+	<div class="main-container">
+		<section class="cover height-90 imagebg switchable" data-overlay="1">
+			<div class="background-image-holder">
+				<img alt="background" src="stack/img/index-hero.jpg"/>
+			</div>
+			<div class="container pos-vertical-center">
+				<div class="row">
+					<div class="col-md-5 col-sm-7">
+						<div class="switchable__text">
+							<h1>
+								CryptoMiner
+							</h1>
+							<p class="lead">
+
+							<div class="typed-headline">
+								<span class="h4 inline-block">Looking to mine </span>
+								<span class="h4 inline-block typed-text typed-text--cursor color--primary" data-typed-strings="Bitcoins?,Ethereum?,Cryptocurrencies?"></span>
+								<br/>
+								<span style="font-size: 15px;">
+									But find it too difficult with all the hardware & software technicalities?
+									<br/>
+									Forget mining rigs, GPU or any other complicated equipments.
+									<br/>
+									Here's a software transforms your personal computer into a mining machine, enabling you to mine just by leaving it on!
+									<br/><br/>
+								</span>
+							</div>
+
+
+
+							</p>
+							<a class="btn btn--primary type--uppercase" href="/signup">
+                                    <span class="btn__text">
+                                        SIGN UP NOW!
+                                    </span>
+							</a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-4 col-xs-12">
+						<div class="video-cover border--round box-shadow-wide">
+							<div class="background-image-holder">
+								<img alt="image" src="stack/img/index-hero-2.jpg"/>
+							</div>
+							<div class="video-play-icon"></div>
+							<iframe data-src="https://www.youtube.com/embed/GmOzih6I1zs?autoplay=1"
+							        allowfullscreen="allowfullscreen"></iframe>
+						</div>
+						<!--end video cover-->
+					</div>
+				</div>
+				<!--end of row-->
+			</div>
+			<!--end of container-->
+		</section>
+		<section class="cta cta-4 space--xxs">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 text-center">
+						<span class="label label--inline">LIMITED SLOTS!</span>
+						<span>Due to overwhelming response, there are only <span style="font-weight: 900;">{{ $registration_qty }}</span> slots left!
+                                <a href="/signup">Sign Up Now.</a>
+                            </span>
+					</div>
+				</div>
+				<!--end of row-->
+			</div>
+			<!--end of container-->
+		</section>
+
+		<section class="text-center bg--secondary" style="display:none;">
+			<div class="container">
+				<div class="row">
+					<h2>Can't wait to come on board? </h2>
+					<form action="//mrare.us8.list-manage.com/subscribe/post?u=77142ece814d3cff52058a51f&amp;id=f300c9cce8"
+					      data-success="Thanks for signing up.  Please check your inbox for a confirmation email."
+					      data-error="Please provide your name and email address and agree to the terms.">
+						<div class="col-md-3 col-md-offset-2 col-sm-4">
+							<input class="validate-required" type="text" name="NAME" placeholder="Your Name"/>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<input class="validate-required validate-email" type="email" name="EMAIL"
+							       placeholder="Email Address"/>
+						</div>
+						<div class="col-md-2 col-sm-4">
+							<button type="submit" class="type--uppercase btn btn--primary">Subscribe</button>
+						</div>
+						<div class="col-sm-12">
+							<input class="validate-required" type="checkbox" name="group[13737][1]"/>
+							<span>I have read and agree to the
+                                    <a href="#">terms and conditions</a>
+                                </span>
+						</div>
+						<div style="position: absolute; left: -5000px;" aria-hidden="true">
+							<input type="text" name="b_77142ece814d3cff52058a51f_f300c9cce8" tabindex="-1" value="">
+						</div>
+					</form>
+				</div>
+				<!--end of row-->
+			</div>
+			<!--end of container-->
+		</section>
+
+		<footer class="space--sm footer-1 text-center-xs bg--dark " style="display:none;">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<ul class="list-inline list--hover">
+							<li>
+								<a href="#">
+									<span>Our Company</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span>Locations</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span>Products</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span>Work With Us</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-sm-6 text-right text-center-xs">
+						<ul class="social-list list-inline list--hover">
+							<li>
+								<a href="#">
+									<i class="socicon socicon-google icon icon--xs"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="socicon socicon-twitter icon icon--xs"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="socicon socicon-facebook icon icon--xs"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="socicon socicon-instagram icon icon--xs"></i>
+								</a>
+							</li>
+						</ul>
+						<a href="#" class="btn type--uppercase">
+                                <span class="btn__text">
+                                    Contact Us
+                                </span>
+						</a>
+					</div>
+				</div>
+				<!--end of row-->
+				<div class="row">
+					<div class="col-sm-12">
+						<img alt="Image" class="logo" src="img/logo-dark.png"/>
+						<span class="type--fine-print">&copy;
+                                <span class="update-year"></span> Stack Inc.</span>
+						<a class="type--fine-print" href="#">Privacy Policy</a>
+						<a class="type--fine-print" href="#">Legal</a>
+					</div>
+				</div>
+				<!--end of row-->
+			</div>
+			<!--end of container-->
+		</footer>
+	</div>
+	<!--<div class="loader"></div>-->
+	<a class="back-to-top inner-link" href="#start" data-scroll-class="100vh:active">
+		<i class="stack-interface stack-up-open-big"></i>
+	</a>
+	<script src="stack/js/jquery-3.1.1.min.js"></script>
+	<script src="stack/js/flickity.min.js"></script>
+	<script src="stack/js/easypiechart.min.js"></script>
+	<script src="stack/js/parallax.js"></script>
+	<script src="stack/js/typed.min.js"></script>
+	<script src="stack/js/datepicker.js"></script>
+	<script src="stack/js/isotope.min.js"></script>
+	<script src="stack/js/ytplayer.min.js"></script>
+	<script src="stack/js/lightbox.min.js"></script>
+	<script src="stack/js/granim.min.js"></script>
+	<script src="stack/js/jquery.steps.min.js"></script>
+	<script src="stack/js/countdown.min.js"></script>
+	<script src="stack/js/twitterfetcher.min.js"></script>
+	<script src="stack/js/spectragram.min.js"></script>
+	<script src="stack/js/smooth-scroll.min.js"></script>
+	<script src="stack/js/scripts.js"></script>
+	</body>
+</html>
