99f19084bf7ee416e3daaeeeb3eb3a2f84c8428c
rong <rongronggg@gmail.com>
2017-12-05 13:39:59 +0800

    Web: Poll Xmrpool for stats

---
+++ app/Http/Controllers/StatsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class StatsController extends Controller
+{
+    public function getStats(Request $request){
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get("https://api.xmrpool.net/miner/48WCGXaoL7gUY8fwSxUPgR4VYx4iVTJYEF4jP7Uq4jG26Hz9Gc6QjgU1m7Hht5pBPJbccCyR4khkZD88wSwErkRt2FkmpNH/stats/allWorkers",[]);
+        dump($response);
+
+    }
+}
