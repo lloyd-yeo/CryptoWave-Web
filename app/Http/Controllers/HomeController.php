6e7e00dd12510f1aa2167de1a1d903ab62684ce3
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-02-07 19:13:30 +0800

    Web: Added logging of response

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ use App\UserWallet;
 use Auth;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Log;
 
 class HomeController extends Controller
 {
@@ -268,12 +269,14 @@ class HomeController extends Controller
 	public function pollHashspeed(Request $request) {
 		$client = new \GuzzleHttp\Client();
 		$response = $client->get("http://159.89.197.169:8001/miner/48WCGXaoL7gUY8fwSxUPgR4VYx4iVTJYEF4jP7Uq4jG26Hz9Gc6QjgU1m7Hht5pBPJbccCyR4khkZD88wSwErkRt2FkmpNH/stats/allWorkers", []);
+		Log::info($response);
 		$stats = json_decode($response->getBody(), true);
 		$user = User::find(Auth::user()->id);
 
 		foreach ($stats as $email => $stat) {
 
 			if ($email == $user->email) {
+
 				$half_minute_snapshot = new HalfMinuteSnapshot;
 				$half_minute_snapshot->user_id = $user->id;
 				$half_minute_snapshot->hash_speed = $stat["hash"];
