4840206bde226942d771f3eb0859bb625eb8b705
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-02-27 17:00:30 +0800

    Web: Reformatted miner patch email

--- resources/views/emails/minerpatch.blade.php
+++ resources/views/emails/minerpatch.blade.php
@@ -1,9 +1,6 @@
-Hey {{ $user->name }}!
-<br/>
-CryptoMiner has recently patched the miner to switch to a more efficient pool.
-<br/>
-Please log on to <a href="http://cryptowave.network" target="_blank">http://cryptowave.network</a> & re-install the Miner.
-Any outdated miner's contribution will not be considered after 3rd February 2018.
-<br/>
-Regards,<br/>
-CryptoWave Team
+<p>Hey {{ $user->name }}!</p>
+<p>CryptoMiner has recently patched the miner to switch to a more efficient pool.</p>
+<p>Please log on to <a href="http://cryptowave.network" target="_blank">http://cryptowave.network</a> & re-install the Miner.</p>
+<p>Any outdated miner's contribution will not be considered after 3rd February 2018.</p>
+<p>Regards,</p>
+<p>CryptoWave Team</p>
